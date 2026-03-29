6e12f88e80a5cf84f95f1675af3cbcc3c9cbbfc5
raluca.pascuta7 <raluca.pascuta@stud.ubbcluj.ro>
2026-03-30 00:27:24 +0300

    Add ISavingsRepository interface
    Defines contract for savings account data access operations.
    Contains AddSavingsAccountAsync method for persisting new accounts.

---
+++ src/KarmaBanking.App/Repositories/Interfaces/ISavingsRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using KarmaBanking.App.Models;
+
+namespace KarmaBanking.App.Repositories.Interfaces
+{
+    public interface ISavingsRepository
+    {
+        Task<bool> AddSavingsAccountAsync(SavingsAccount savingsAccount);
+    }
+}
