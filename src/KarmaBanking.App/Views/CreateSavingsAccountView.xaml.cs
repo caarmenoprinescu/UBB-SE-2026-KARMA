3e85ef1cf6b7ea5c11d6f9b419393f75e3978d80
raluca.pascuta7 <raluca.pascuta@stud.ubbcluj.ro>
2026-03-30 02:12:32 +0300

    Add SavingsAccountListView code-behind
    Adds the Page class for SavingsAccountListView.
    Logic is handled by the ViewModel in BA-6.

---
+++ src/KarmaBanking.App/Views/CreateSavingsAccountView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace KarmaBanking.App.Views
+{
+    public sealed partial class CreateSavingsAccountView : Page
+    {
+        public CreateSavingsAccountView()
+        {
+            InitializeComponent();
+        }
+    }
+}
