15a329f2e046a2893ceb734db5700f54a5e04d11
Pășcuță Raluca-Victoria <raluca.pascuta@stud.ubbcluj.ro>
2026-03-30 18:36:46 +0300

    Revert "Raluca/ba 3 post savings endpoint"

--- src/KarmaBanking.App/Repositories/Interfaces/ISavingsRepository.cs
+++ src/KarmaBanking.App/Repositories/Interfaces/ISavingsRepository.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using KarmaBanking.App.Models;
 
 namespace KarmaBanking.App.Repositories.Interfaces
@@ -7,8 +6,5 @@ namespace KarmaBanking.App.Repositories.Interfaces
     public interface ISavingsRepository
     {
         Task<bool> AddSavingsAccountAsync(SavingsAccount savingsAccount);
-        Task<List<SavingsAccount>> GetSavingsAccountsByUserIdAsync(int userId);
-        Task<bool> UpdateSavingsAccountBalanceAsync(int savingsAccountId, decimal amountToAdd);
-        Task<bool> CloseSavingsAccountAsync(int savingsAccountId);
     }
 }
--- src/KarmaBanking.App/Repositories/SavingsRepository.cs
+++ src/KarmaBanking.App/Repositories/SavingsRepository.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using KarmaBanking.App.Models;
 using KarmaBanking.App.Repositories.Interfaces;
@@ -40,84 +39,6 @@ namespace KarmaBanking.App.Repositories
 
             int numberOfRowsAffectedByInsert = await insertSavingsAccountCommand.ExecuteNonQueryAsync();
             return numberOfRowsAffectedByInsert > 0;
-        }
-
-        public async Task<List<SavingsAccount>> GetSavingsAccountsByUserIdAsync(int userId)
-        {
-            const string selectSavingsAccountsByUserIdQuery = @"
-                SELECT id, userId, savingsType, balance, accruedInterest, apy,
-                       maturityDate, accountStatus, createdAt,
-                       accountName, fundingAccountId, targetAmount, targetDate
-                FROM SavingsAccount
-                WHERE userId = @UserId";
-
-            List<SavingsAccount> savingsAccounts = new List<SavingsAccount>();
-
-            using SqlConnection openDatabaseConnection = DatabaseConfig.GetDatabaseConnection();
-            await openDatabaseConnection.OpenAsync();
-
-            using SqlCommand selectSavingsAccountsCommand = new SqlCommand(selectSavingsAccountsByUserIdQuery, openDatabaseConnection);
-            selectSavingsAccountsCommand.Parameters.AddWithValue("@UserId", userId);
-
-            using SqlDataReader savingsAccountReader = await selectSavingsAccountsCommand.ExecuteReaderAsync();
-            while (await savingsAccountReader.ReadAsync())
-            {
-                SavingsAccount savingsAccount = new SavingsAccount
-                {
-                    Id = (int)savingsAccountReader["id"],
-                    UserId = (int)savingsAccountReader["userId"],
-                    SavingsType = savingsAccountReader["savingsType"].ToString(),
-                    Balance = (decimal)savingsAccountReader["balance"],
-                    AccruedInterest = (decimal)savingsAccountReader["accruedInterest"],
-                    Apy = (decimal)savingsAccountReader["apy"],
-                    MaturityDate = savingsAccountReader["maturityDate"] as DateTime?,
-                    AccountStatus = savingsAccountReader["accountStatus"].ToString(),
-                    CreatedAt = (DateTime)savingsAccountReader["createdAt"],
-                    AccountName = savingsAccountReader["accountName"] as string,
-                    FundingAccountId = savingsAccountReader["fundingAccountId"] as int?,
-                    TargetAmount = savingsAccountReader["targetAmount"] as decimal?,
-                    TargetDate = savingsAccountReader["targetDate"] as DateTime?
-                };
-                savingsAccounts.Add(savingsAccount);
-            }
-
-            return savingsAccounts;
-        }
-
-        public async Task<bool> UpdateSavingsAccountBalanceAsync(int savingsAccountId, decimal amountToAdd)
-        {
-            const string updateSavingsAccountBalanceQuery = @"
-                UPDATE SavingsAccount
-                SET balance = balance + @AmountToAdd
-                WHERE id = @SavingsAccountId";
-
-            using SqlConnection openDatabaseConnection = DatabaseConfig.GetDatabaseConnection();
-            await openDatabaseConnection.OpenAsync();
-
-            using SqlCommand updateSavingsAccountBalanceCommand = new SqlCommand(updateSavingsAccountBalanceQuery, openDatabaseConnection);
-            updateSavingsAccountBalanceCommand.Parameters.AddWithValue("@SavingsAccountId", savingsAccountId);
-            updateSavingsAccountBalanceCommand.Parameters.AddWithValue("@AmountToAdd", amountToAdd);
-
-            int numberOfRowsAffectedByUpdate = await updateSavingsAccountBalanceCommand.ExecuteNonQueryAsync();
-            return numberOfRowsAffectedByUpdate > 0;
-        }
-
-        public async Task<bool> CloseSavingsAccountAsync(int savingsAccountId)
-        {
-            const string closeSavingsAccountQuery = @"
-                UPDATE SavingsAccount
-                SET balance = 0,
-                    accountStatus = 'Closed'
-                WHERE id = @SavingsAccountId";
-
-            using SqlConnection openDatabaseConnection = DatabaseConfig.GetDatabaseConnection();
-            await openDatabaseConnection.OpenAsync();
-
-            using SqlCommand closeSavingsAccountCommand = new SqlCommand(closeSavingsAccountQuery, openDatabaseConnection);
-            closeSavingsAccountCommand.Parameters.AddWithValue("@SavingsAccountId", savingsAccountId);
-
-            int numberOfRowsAffected = await closeSavingsAccountCommand.ExecuteNonQueryAsync();
-            return numberOfRowsAffected > 0;
         }
     }
 }
