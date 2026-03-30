c033bb8a45fa4f449b400822f3b526490b3f4213
CarmenOprinescu <carmen.oprinescu@stud.ubbcluj.ro>
2026-03-30 11:30:14 +0300

    BA-29 finished

--- src/KarmaBanking.App/Repositories/LoanRepository.cs
+++ src/KarmaBanking.App/Repositories/LoanRepository.cs
@@ -1,10 +1,11 @@
 
 using System.Data.SqlClient;
 using System.Collections.Generic;
+using System.Linq;
 
 public class LoanRepository
 {
-   public List<Loan> getAllLoans()
+    public List<Loan> getAllLoans()
     {
         List<Loan> loans = new List<Loan>();
         using (SqlConnection connection = new SqlConnection(DatabaseConfig.connectionString))
@@ -36,5 +37,40 @@ public class LoanRepository
         }
         return loans;
     }
-   
+    public List<Loan> getByUser(int userID)
+    {
+        List<Loan> loans = new List<Loan>();
+
+        using (SqlConnection connection = new SqlConnection(DatabaseConfig.connectionString))
+        {
+            connection.Open();
+
+            string query = "SELECT * FROM Loans l WHERE l.userId = userID";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Loan loan = new Loan
+                {
+                    id = (int)reader["id"],
+                    userId = (int)reader["userId"],
+                    loanType = reader["loanType"].ToString(),
+                    principal = (decimal)reader["principal"],
+                    outstandingBalance = (decimal)reader["outstandingBalance"],
+                    interestRate = (decimal)reader["interestRate"],
+                    monthlyInstallment = (decimal)reader["monthlyInstallment"],
+                    remainingMonths = (int)reader["remainingMonths"],
+                    loanStatus = reader["loanStatus"].ToString()
+                };
+
+                loans.Add(loan);
+            }
+            return loans;
+        }
+
+
+
+    }
 }
