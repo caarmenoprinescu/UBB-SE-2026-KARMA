32cf62329cbba3171dcbdba01cb05809d8f1d871
MatiaOpris <rev.oprismatia@gmail.com>
2026-04-01 11:32:16 +0300

    Changes in InvestmentRepository

--- src/KarmaBanking.App/Repositories/Interfaces/IInvestmentRepository.cs
+++ src/KarmaBanking.App/Repositories/Interfaces/IInvestmentRepository.cs
@@ -1,4 +1,9 @@
 ﻿using KarmaBanking.App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace KarmaBanking.App.Repositories.Interfaces
 {
