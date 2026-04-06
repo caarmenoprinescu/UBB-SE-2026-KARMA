d24dcad9e6f7db66dcc0d0b723ec5df85d616eeb
Mara <mara.panzaru@gmail.com>
2026-04-06 15:32:04 +0300

    Payment preview

--- src/KarmaBanking.App/Data/DatabaseConfig.cs
+++ src/KarmaBanking.App/Data/DatabaseConfig.cs
@@ -3,7 +3,7 @@ using Microsoft.Data.SqlClient;
 public static class DatabaseConfig
 {
     public static readonly string ConnectionString =
-        "Server=DESKTOP-FNDDLP7\\SQLEXPRESS;Database=KarmaBankingDb;Trusted_Connection=True;TrustServerCertificate=True;";
+        "Server=(localdb)\\MSSQLLocalDB;Database=KarmaBankingDb;Trusted_Connection=True;TrustServerCertificate=True;";
     public static SqlConnection GetDatabaseConnection()
     {
         return new SqlConnection(ConnectionString);
